74d533c63deb5ee2232be352bc8e27c5a31553d7
John-Khalil <johnadelkhalil@gmail.com>
2022-08-02 04:26:14 +0200

    now its safe to say we're on the right track

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 
 import './App.css';
 
-
+import ColorPalette from "./componenets/ColorPalette";
 
 
 
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!!
-    </h1>
+    <ColorPalette/>
   )
 }
--- src/componenets/ColorPalette.js
+++ src/componenets/ColorPalette.js
@@ -4,9 +4,15 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 
+import '../App.css';
+
 
 export default function ColorPalette() {
-  return (
-    <div>ColorPalette</div>
-  )
+
+
+    const finalColor=useSelector(state=>state.colorPalette.value);
+
+    return (
+        <div className="felx  w-1/2 rounded-lg text-center" style={{backgroundColor:finalColor}}>ColorPalette</div>
+    )
 }
