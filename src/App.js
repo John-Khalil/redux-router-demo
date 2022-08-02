83448a7873cd96ffc4280cb9311dd1fc402f52b9
John-Khalil <johnadelkhalil@gmail.com>
2022-08-02 06:10:35 +0200

    react router added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 
 import './App.css';
 
-import ColorPalette from "./componenets/ColorPalette";
-import ColorSelector from "./componenets/ColorSelector";
 
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AnotherPage from './AnotherPage';
+import MainApp from './MainApp';
 
 export default function App() {
   return (
 
-    <>
-      <ColorPalette/>
-
-      <ColorSelector/>
-      
-    </>
+    <Router>
+      <nav>
+        <Link to="/"> <div className="felx  m-1 p-1 rounded-lg text-center inline-block" style={{backgroundColor:"green"}}>main page</div> </Link>
+        <Link to="/page 2"> <div className="felx m-1 p-1 rounded-lg text-center inline-block" style={{backgroundColor:"green"}}>another page</div> </Link>
+        
+      </nav>
+      <br></br>
+      <Routes>
+        <Route path="/" element={<MainApp />} />
+        <Route path="*" element={<AnotherPage />} />
+      </Routes>
+    </Router>
   )
 }
---
+++ src/MainApp.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import ColorPalette from "./componenets/ColorPalette";
+import ColorSelector from "./componenets/ColorSelector";
+
+
+
+export default function MainApp() {
+  return (
+
+    <>
+      <ColorPalette/>
+
+      <ColorSelector/>
+      
+    </>
+  )
+}
