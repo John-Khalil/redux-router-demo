f79b633fc03043040ca961b5c4baeae9829006a4
John-Khalil <johnadelkhalil@gmail.com>
2022-08-02 17:49:41 +0200

    layout added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@
 import './App.css';
 
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AnotherPage from './AnotherPage';
 import MainApp from './MainApp';
+import Layout from './layouts/Layout';
 
 export default function App() {
   return (
 
-    <Router>
-      <nav>
+    <BrowserRouter>
+      {/* <nav>
         <Link to="/"> <div className="felx  m-1 p-1 rounded-lg text-center inline-block" style={{backgroundColor:"green"}}>main page</div> </Link>
         <Link to="/page 2"> <div className="felx m-1 p-1 rounded-lg text-center inline-block" style={{backgroundColor:"green"}}>another page</div> </Link>
         
       </nav>
-      <br></br>
+      <br></br> */}
+      {/* i know this isnt the best practice to comment code but this is for demonestrating purpose  */}
       <Routes>
-        <Route path="/" element={<MainApp />} />
-        <Route path="*" element={<AnotherPage />} />
+        <Route path='/' element={<Layout/>}>
+          <Route path="home" element={<MainApp />} />
+          <Route path="another-page" element={<AnotherPage />} />
+          <Route path="*" element={<MainApp />} />
+        </Route>
+        
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
